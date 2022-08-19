397c01f3f7b4350869c0ec5bde9001432dbc578d
TiwiGonzalez <tiwikakigonzalez2010@gmail.com>
2022-08-18 21:05:33 -0300

    primer desafio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// ingrese numeros e imprimalos
+// Cuando ingreses 0 termina,
+// al final imprime la cantidad de numeros ingresados
+
+// let i = 0;
+// let num;
+// do {
+//     num = parseInt(prompt("Ingrese el numero"))
+//     document.write("el numero es: " +num+ "<br>")
+//     i++;
+// } while(num!=0);
+// document.write("<br> La cantidad de numero es" + i );
+
+
+// Probe For para ver si lo podia aplicar espero que este bien todo!
+
+let num;
+for(i=0;num!=0;i++){
+    num = parseInt(prompt("Ingrese el numero"))
+    document.write("el numero es: " +num+ "<br>")
+} ;
+document.write("<br> La cantidad de numeros es" + i );
